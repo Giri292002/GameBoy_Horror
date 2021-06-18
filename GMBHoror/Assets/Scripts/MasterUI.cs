59d5b8b6c05f3fdd841420b97a5926cce6feb457
Giri <pbgiridhara@gmail.com>
2021-06-18 08:56:00 +0530

    Finished Main Menu

--- GMBHoror/Assets/Scripts/MasterUI.cs
+++ GMBHoror/Assets/Scripts/MasterUI.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MasterUI : MonoBehaviour
 {
     [SerializeField]
     Animation GameOverAnim = null;
     public GameObject GameOverScreen = null;
+    public Text GameOverScore;
+
+    public GameObject Key = null;
+    public Text Score = null;
+    private int scoreNumber;
+
+    public GameObject Credits;
+
+
 
     public void GoToLevel(string LevelName)
     {
@@ -21,8 +31,24 @@ public class MasterUI : MonoBehaviour
 
     public void GameOver()
     {
+        GameOverScore.text = "SCORE: " + scoreNumber;
         GameOverScreen.SetActive(true);
         GameOverAnim.Play();
-        //TODO: Get Score and Put it here
+    }
+
+    public void SetKeyVisiblity(bool active)
+    {
+        Key.SetActive(active);
+    }
+
+    public void SetScore(int score)
+    {
+        scoreNumber = score;
+        Score.text = "ROOMS CLEARED: " + score;
+    }
+
+    public void ShowCredits(bool visible)
+    {
+        Credits.SetActive(visible);
     }
 }
